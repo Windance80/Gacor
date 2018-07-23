2e2447735b2a290cc60396002d0e62434409eac6
sandy theodorus <sandy.theodorus@gmail.com>
2018-07-23 12:02:38 +0700

    Change bottom Nav

--- app/src/main/java/com/kingstone/smith/gacor/MainActivity.java
+++ app/src/main/java/com/kingstone/smith/gacor/MainActivity.java
@@ -18,14 +18,14 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.title_gacor);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_heatmap);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+//                case R.id.navigation_notifications:
+//                    mTextMessage.setText(R.string.title_notifications);
+//                    return true;
             }
             return false;
         }
