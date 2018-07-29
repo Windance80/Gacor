920ddedb699710298f681909aea391ca6cdfc83b
sandy theodorus <sandy.theodorus@gmail.com>
2018-07-29 15:30:21 +0700

    add fragment heat spot

--- app/src/main/java/com/kingstone/smith/gacor/MainActivity.java
+++ app/src/main/java/com/kingstone/smith/gacor/MainActivity.java
@@ -1,8 +1,11 @@
 package com.kingstone.smith.gacor;
 
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -11,17 +14,22 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_gacor:
                     mTextMessage.setText(R.string.title_gacor);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_heatmap);
+                case R.id.navigation_heatspot:
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+
+//                    mTextMessage.setText(R.string.title_heatspot);
                     return true;
 //                case R.id.navigation_notifications:
 //                    mTextMessage.setText(R.string.title_notifications);
@@ -37,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        findViewById(R.id.co)
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
